a245618c257fd1dd8c610e210344320a8c89b8c3
r0binsahin <robin.sahin88@gmail.com>
2024-07-31 13:24:18 +0200

    build run
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

---
+++ build/app.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const logger_1 = require("./logger");
+const app = (0, express_1.default)();
+const port = 8080;
+app.use(express_1.default.json());
+app.get('/', (_req, res) => {
+    res.status(200).send('Hello from Index1');
+    logger_1.logger.info('Index page!');
+});
+app.get('/status', (_req, res) => {
+    res.status(200).send('Hello from Rookies');
+    logger_1.logger.info('Status page!');
+});
+app.post('/payments', (req, res) => {
+    const { carId, amount } = req.body;
+    console.log(carId);
+    console.log(amount);
+    if (typeof amount !== 'number' || !Number.isInteger(amount)) {
+        logger_1.logger.log({
+            message: 'Invalid amount. It must be an integer.',
+            level: 'error',
+        });
+        return res
+            .status(400)
+            .json({ error: 'Invalid amount. It must be an integer.' });
+    }
+    const random = Math.random();
+    if (random > 0.5) {
+        logger_1.logger.log({
+            message: 'Invalid random. It must be less than 0.5.',
+            level: 'error',
+        });
+    }
+    res.status(200).json('Payment success');
+    logger_1.logger.info({
+        level: 'info',
+        message: 'Payment processed successfully',
+        carId,
+        amount,
+    });
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
